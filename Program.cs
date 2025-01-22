81fdae6448dfacd957fa8abd836f22c51c5558e2
Sadia Rashid <30407176+sadiachaan@users.noreply.github.com>
2025-01-22 18:53:25 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyTaskManager;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    List<Task> taskList = new List<Task>();
+        //Initial sample task
+        taskList.Add(new Task(1, "C# 12 project", DateTime.Now.AddDays(3)));
+
+         DisplayMenu();
+
+        while(true)
+        {
+            Console.WriteLine("Enter your choice: ");
+            string choice = Console.ReadLine();
+            switch (choice) {
+                case "1":
+                    AddTask();
+                    break;
+                case "2":
+                    ReadTasks();
+                    break;
+                case"3":
+                    MarkAsCompleted();
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting...")
+                    return;
+                default:
+                    Console.WriteLine("Invalid Choice.");
+                    break;
+            }
+
+
+        Console.WriteLine("");
+    }
+
+
+        private static void AddTask()
+    {
+        int taskId = taskList.Count+ 1 ;
+        Console.WriteLine("Enter task description : ");
+        string description = Console.ReadLine();
+        Console.Write("Enter due date (YYYY-MM-DD): ");
+        DateTime dueDate = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine("Task Added!");
+    }
+
+    void ReadTask()
+    {
+        Console.WriteLine("***Task List***");
+        Console.WriteLine("Task ID Description");
+        Console.WriteLine();
+    }
+
+    private static void DisplayMenu()
+    {
+        Console.WriteLine("****My Task Manager****");
+        Console.WriteLine("1. Add Task");
+        Console.WriteLine("2. Read Tasks");
+        Console.WriteLine("3. Mark task as Completed.");
+        Console.WriteLine("4. Exit");
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace MyTaskManager
+{
+    public class Task
+    {
+        int TaskId;
+        string Description;
+        DateTime DueDate;
+        bool IsCompleted; 
+
+        //Add constructor to simplify task Creation
+        public Task (int taskId, string description, DateTime dueDate)
+        {      
+            TaskId = taskId;
+            Description = description;
+            DueDate = dueDate;
+            //IsComplete = false;
+     }
+         }
+}
